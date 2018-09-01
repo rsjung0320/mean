fc252e522c232675fcde2e45c68d6100e1f7ec62
raesung <rsjung0320@gmail.com>
2018-09-01 14:52:46 +0900

    unit 27, there are information of rxjs in comment

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -16,17 +16,29 @@ export class PostListComponent implements OnInit, OnDestroy {
   //   {title: 'Third Post', content: 'This is the third post\'s content'}
   // ];
   posts: Post[] = []; // post-create에서 @Output 한 것을 상위 app.component를 걸쳐서 받는다.
-  private postsSub: Subscription;
+  private postsSub: Subscription; // observer와 같다고 생가하면 될 듯.
 
   constructor(public postsService: PostsService) {
   }
 
   ngOnInit() {
+
     // this.posts = this.postsService.getPosts();
+    // 리스트이기 때문에 Observer가 되야 한다. 상황이 변경이 되면 첫번째, param에서 해당 내역을 처리하고, 두번째 param에서는 error를 세번째 param에는 완료 후 처리 로직이 들어간다.
     this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      // 200: Success
       this.posts = posts;
+      console.log(`posts : `, posts);
+    }, (err) => {
+      // fail
+      console.log(err);
+    }, () => {
+      // 200: Success,
+      // TODO 좀 더 알아 볼 것, 후처리가 아니라, Observable이 자신을 종료, 소멸 했을 때, 이벤트를 emit 하는 것을 받아 주는 것 같다.
+      console.log('Observable / Subject is over / the end');
     });
   }
+
 
   ngOnDestroy() {
     // Subscription 과 메모리 leak을 없앤다.
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -8,7 +8,7 @@ import { Post } from './post.model';
 export class PostsService {
   // 이러한 방식이 외부에서 접근이 되지 않고, javascript의 외부에서 접근 못하게 막으며, 본래의 posts의 변수는 건들 수 없다고 한다.
   private posts: Post[] = [];
-  private postsUpdated = new Subject<Post[]>();
+  private postsUpdated = new Subject<Post[]>(); // subject는 observable과 같다고 생각하면 된다.
 
   getPosts() {
     return this.posts;
@@ -21,6 +21,7 @@ export class PostsService {
   addPost (title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    // 위에서 값을 넣고, observer들이 알 수 있도록 이벤트를 emit 한다.
     this.postsUpdated.next([...this.posts]);
   }
 }
