d15b9bc1e4a1e51bf5bfd23fd1d31346eae00701
raesung <rsjung0320@gmail.com>
2018-09-03 16:07:23 +0900

    unit 39

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   // 보안 이슈는 아니고, 설정하면 된다고 한다.
@@ -8,6 +12,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Header', "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully!',
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
