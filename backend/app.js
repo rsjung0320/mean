70757f3f6922a66abf08818d3832459475f7dfe6
raesung <rsjung0320@gmail.com>
2018-09-03 17:53:04 +0900

    unit 49, mongoose model 생성

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
-
+// NqiqwFKyG1d7wDqv
 app.post("/api/posts", (req, res, next) => {
   const post = req.body;
   console.log(post);
---
+++ backend/models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  // title: {type: String, require: true, default: 'Hello'},
+  title: {type: String, require: true},
+  content: {type: String, require: true}
+});
+
+// 1번 째는 param은 모델 이름, 2번 째는 스키마 정의
+module.exports = mongoose.model('Post', postSchema);
