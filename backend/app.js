def8b8e6a174e9e3403475600d12411e79b4d79f
raesung <rsjung0320@gmail.com>
2018-09-03 15:16:04 +0900

    unit 35, nodemon은 node.js에서 수정 후 save를 누르면 자동으로 restart 해 준다.

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   // console.log('Second middleware');
   // next();
-  res.send('Hello from express!');
+  res.send('Hello from express!!');
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,53 @@
  // require는 node.js에서 import를 뜻한다. 그래서 http관련을 가져온다.
- const http = require('http');
- const app = require('./backend/app');
+ const app = require("./backend/app");
+ const debug = require("debug")("node-angular");
+ const http = require("http");
 
- const port = process.env.PORT || 3000;
+ const normalizePort = val => {
+   var port = parseInt(val, 10);
 
- app.set('port', port);
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+
+   return false;
+ };
+
+ const onError = error => {
+   if (error.syscall !== "listen") {
+     throw error;
+   }
+   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+   switch (error.code) {
+     case "EACCES": // Permission denied
+       console.error(bind + " requires elevated privileges");
+       process.exit(1);
+       break;
+     case "EADDRINUSE": // Address already in use
+       console.error(bind + " is already in use");
+       process.exit(1);
+       break;
+     default:
+       throw error;
+   }
+ };
+
+ const onListening = () => {
+   const addr = server.address();
+   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+   debug("Listening on " + bind);
+ };
+
+ const port = normalizePort(process.env.PORT || "3000");
+ app.set("port", port);
+
  const server = http.createServer(app);
-
+ server.on("error", onError);
+ server.on("listening", onListening);
  server.listen(port);
