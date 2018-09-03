52a409c5332531417c0c68cd77f30bbf9c4a2720
raesung <rsjung0320@gmail.com>
2018-09-03 15:04:06 +0900

    unit 34, express 설치

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('First middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  // console.log('Second middleware');
+  // next();
+  res.send('Hello from express!');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
  // require는 node.js에서 import를 뜻한다. 그래서 http관련을 가져온다.
  const http = require('http');
+ const app = require('./backend/app');
 
- const server = http.createServer((req, res) => {
-  res.end('This is my first response');
+ const port = process.env.PORT || 3000;
 
- });
+ app.set('port', port);
+ const server = http.createServer(app);
 
- server.listen(process.env.PORT || 3000);
+ server.listen(port);
