a9d321e312cab5e1ab589f9490a52e9f1ea5b24a
raesung <rsjung0320@gmail.com>
2018-09-04 10:26:31 +0900

    unit 53, fetch 성공

--- backend/app.js
+++ backend/app.js
@@ -41,22 +41,15 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'faef2f2f1',
-      title: "First server-side post",
-      content: "First content1"
-    },
-    {
-      id: 'faef2f2f2',
-      title: "Second server-side post",
-      content: "Second content1"
-    }
-  ];
-  return res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      return res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    })
+    .catch();
+
   // next()는 이 함수를 끝낸다는 뜻이다. 하지만 우리는 계속해서 해야하니 쓰면 안된다.
 });
 
