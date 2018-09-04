0a87156d6d59466e4a10092739565a4a44c061de
raesung <rsjung0320@gmail.com>
2018-09-04 10:33:51 +0900

    unit 54, 몽고 db에서 _id로 생성하는대 angular에서 모델에서 _id로 만들었다. 그것을 pipe와 map 바로 잡음

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,38 +1,50 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
 // 아래 @Injectable({providedIn: 'root'}) 을 안쓰려면 app.module.ts에 providers에 추가 해야 함
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   // 이러한 방식이 외부에서 접근이 되지 않고, javascript의 외부에서 접근 못하게 막으며, 본래의 posts의 변수는 건들 수 없다고 한다.
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>(); // subject는 observable과 같다고 생각하면 된다.
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
-        this.postsUpdated.next([...this.posts]);
+    this.http.get<{ message: string, posts: any }>(
+      'http://localhost:3000/api/posts'
+    )
+    .pipe(map((postData) => {
+      return  postData.posts.map(post => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        };
       });
+    }))
+    .subscribe(transformedPosts => {
+      this.posts = transformedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
 
-  addPost (title: string, content: string) {
-    const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
-    .subscribe((responseData) => {
-      console.log(responseData.message);
-      this.posts.push(post);
-      // 위에서 값을 넣고, observer들이 알 수 있도록 이벤트를 emit 한다.
-      this.postsUpdated.next([...this.posts]);
-    });
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        // 위에서 값을 넣고, observer들이 알 수 있도록 이벤트를 emit 한다.
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
