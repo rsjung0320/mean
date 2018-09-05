2db4187611320405f1430192d33a37b7443bf13a
raesung <rsjung0320@gmail.com>
2018-09-05 17:58:56 +0900

    unit 96. Creating the User Model

---
+++ backend/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const uniqueValidator = requestAnimationFrame('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  // title: {type: String, require: true, default: 'Hello'},
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+// 1번 째는 param은 모델 이름, 2번 째는 스키마 정의
+module.exports = mongoose.model("User", userSchema);
