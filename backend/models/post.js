57a057759e484d293580b45b6f1b7df033eb4d84
raesung <rsjung0320@gmail.com>
2018-09-06 15:34:08 +0900

    unit 113. Adding a Reference to the Model

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,7 +4,8 @@ const postSchema = mongoose.Schema({
   // title: {type: String, require: true, default: 'Hello'},
   title: { type: String, require: true },
   content: { type: String, require: true },
-  imagePath: { type: String, require: true }
+  imagePath: { type: String, require: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true }
 });
 
 // 1번 째는 param은 모델 이름, 2번 째는 스키마 정의
