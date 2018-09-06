3c0873574938d3d3315fd9f83d15ae3dc7f8bdd5
raesung <rsjung0320@gmail.com>
2018-09-06 10:41:49 +0900

    unit 100. Implementing SPA Authentication, jwt 추가

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -25,6 +26,39 @@ router.post("/signup", (req, res, next) => {
         });
       });
   });
+});
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      const token = jwt.sign({
+        email: result.email,
+        userId: result._id
+      },
+      // 이것은 나의 key이므로 외부에 노출 되면 안된다.
+      'secret_my_first_key',
+      {
+        expiresIn: '1h'
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    });
 });
 
 module.exports = router;
