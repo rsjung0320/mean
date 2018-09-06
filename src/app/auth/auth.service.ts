56d9dd76c57d2c42c2a3eb30fc1f4cdc2694a609
raesung <rsjung0320@gmail.com>
2018-09-06 17:57:01 +0900

    unit  120. Testing Different Places to Handle Errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,9 +32,11 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post(
-      'http://localhost:3000/api/user/signup/', authData)
-      .subscribe(response => {
+    this.http.post('http://localhost:3000/api/user/signup/', authData)
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      }, err => {
+        this.authStatusListener.next(false);
       });
   }
 
@@ -56,6 +58,8 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
+      }, err => {
+        this.authStatusListener.next(false);
       });
   }
 
