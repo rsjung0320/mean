02db5ccaadecbe4d4f168f99d6bf98009db421cd
raesung <rsjung0320@gmail.com>
2018-09-07 13:30:04 +0900

    unit 132. Fixing an Update Bug

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,6 +1,5 @@
 const Post = require("../models/post");
 
-
 exports.createPost = (req, res, next) => {
   const url = req.protocol + "://" + req.get("host");
   // 아직은 mongodb와 연결은 되지 않았다. new Post를 한 순간 몽고 DB에서 ID를 생성하는 것을 확인하였다.
@@ -12,21 +11,23 @@ exports.createPost = (req, res, next) => {
     creator: req.userData.userId
   });
   // 실제 몽고 DB에 생성된 Data를 넣는다.
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added successfully!",
-      post: {
-        id: createdPost._id,
-        ...createdPost
-      }
-    });
-  })
+  post
+    .save()
+    .then(createdPost => {
+      res.status(201).json({
+        message: "Post added successfully!",
+        post: {
+          id: createdPost._id,
+          ...createdPost
+        }
+      });
+    })
     .catch(error => {
       res.status(500).json({
-        message: 'Creating a post failed!'
+        message: "Creating a post failed!"
       });
     });
-}
+};
 
 exports.updatePost = (req, res, next) => {
   let imagePath = req.body.imagePath;
@@ -42,24 +43,25 @@ exports.updatePost = (req, res, next) => {
     creator: req.userData.userId
   });
   // 1번 째는 where 절이고, 2번째는 update할 값이다.
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-    // nModified 가 0이면 수정이 안된 것이고, 1이면 수정이 완료 된 것이다.
-    if (result.nModified > 0) {
-      res.status(200).json({
-        mssage: "Update successfully!"
-      });
-    } else {
-      res.status(401).json({
-        mssage: "Not authorized!"
-      });
-    }
-  })
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    .then(result => {
+      // nModified 가 0이면 수정이 안된 것이고, 1이면 수정이 완료 된 것이다.
+      if (result.n > 0) {
+        res.status(200).json({
+          mssage: "Update successfully!"
+        });
+      } else {
+        res.status(401).json({
+          mssage: "Not authorized!"
+        });
+      }
+    })
     .catch(error => {
       res.status(500).json({
         message: "Couldn't Update post!"
       });
     });
-}
+};
 
 exports.getPosts = (req, res, next) => {
   // string 앞에 +를 붙이면 number로 convert 된다.
@@ -68,9 +70,7 @@ exports.getPosts = (req, res, next) => {
   const postQuery = Post.find();
   let fetchedPosts;
   if (pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
   postQuery
     .then(documents => {
@@ -91,38 +91,40 @@ exports.getPosts = (req, res, next) => {
     });
 
   // next()는 이 함수를 끝낸다는 뜻이다. 하지만 우리는 계속해서 해야하니 쓰면 안된다.
-}
+};
 
 exports.getPost = (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  })
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found!" });
+      }
+    })
     .catch(error => {
       res.status(500).json({
         message: "Fetching posts failed!"
       });
     });
-}
+};
 
 exports.deletePost = (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
-    if (result.n > 0) {
-      res.status(200).json({
-        mssage: "Post deleted!"
-      });
-    } else {
-      res.status(401).json({
-        mssage: "Not authorized!"
-      });
-    }
-  })
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then(result => {
+      if (result.n > 0) {
+        res.status(200).json({
+          mssage: "Post deleted!"
+        });
+      } else {
+        res.status(401).json({
+          mssage: "Not authorized!"
+        });
+      }
+    })
     .catch(error => {
       res.status(500).json({
         message: "Couldn't delete posts!"
       });
     });
-}
+};
