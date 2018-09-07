6c078ffa5d0fa91e3c37d7aca380cb712a70b648
raesung <rsjung0320@gmail.com>
2018-09-07 13:15:21 +0900

    unit 129. Separating the Middleware

---
+++ backend/middleware/file.js
@@ -0,0 +1,29 @@
+// Multer는 파일 업로드를 위해 사용되는 multipart/form-data 를 다루기 위한 node.js 의 미들웨어 입니다. 효율성을 최대화 하기 위해 busboy 를 기반으로 하고 있습니다.
+const multer = require("multer");
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg"
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname
+      .toLowerCase()
+      .split(" ")
+      .join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  }
+});
+
+module.exports = multer({ storage: storage }).single("image");
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,52 +1,16 @@
 const express = require("express");
-// Multer는 파일 업로드를 위해 사용되는 multipart/form-data 를 다루기 위한 node.js 의 미들웨어 입니다. 효율성을 최대화 하기 위해 busboy 를 기반으로 하고 있습니다.
-const multer = require("multer");
 
-const PostController = require('../controllers/posts');
+const PostController = require("../controllers/posts");
 
-const checkAuth = require('../middleware/check-auth');
+const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  }
-});
-
-router.post(
-  "",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  PostController.createPost
-);
+router.post("", checkAuth, extractFile, PostController.createPost);
 
 // update는
-router.put(
-  "/:id",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  PostController.updatePost
-);
+router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
 router.get("", PostController.getPosts);
 
